dea7f5e944dcb697872b9c15625d762e4e198e1a
beckboan <beckmboan@gmail.com>
2024-09-16 18:11:13 -0400

    Rename project.

---
+++ workspace/001Tasks/Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,22 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/beckm/OneDrive/WinDev/stm32-FreeRTOS/stm32-dev/workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
